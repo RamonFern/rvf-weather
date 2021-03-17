7d0989a28989172dca9bef9adb8635f24be04e29
RamonFern <ramonfernandes9@gmail.com>
2021-03-16 22:55:47 -0300

    Adicionando home effects

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,17 +4,20 @@ import { HomePage } from './home.page';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { homeReducer } from './state/home.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { HomeEffects } from './state/home.effects';
 
 
 
 @NgModule({
-  declarations: [
-    HomePage
-  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     StoreModule.forFeature('home', homeReducer),
-  ]
+    EffectsModule.forFeature([HomeEffects]),
+  ],
+  declarations: [
+    HomePage
+  ],
 })
 export class HomeModule { }
